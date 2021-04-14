879271601507b29eb53408da2081a1fcb7a6d7f9
justin Polacek <justinpolacek@gmail.com>
2021-04-14 13:56:55 -0800

    new updates

---
+++ app.js
@@ -0,0 +1,16 @@
+const menuToggle = document.querySelector('.toggle');
+const showCase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showCase.classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+
